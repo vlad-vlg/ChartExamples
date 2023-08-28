d286c821dcce70315cfe94b04e59cce33e182777
Влад Иванович <vlad@ya.ru>
2023-08-28 17:52:12 +0300

    Создание основвы программы

---
+++ ChartExamples.py
@@ -0,0 +1,11 @@
+# Примеры построения графиков
+
+import tkinter as tk
+
+# Создание главного окна
+window = tk.Tk()
+window.geometry('450x450+300+200')
+window.title('Примеры построения графиков')
+
+# Запуск цикла mainloop
+window.mainloop()
