627da6133cac25cf8c252a1dc8bcda0835a5193a
Влад Иванович <vlad@ya.ru>
2023-08-30 15:31:17 +0300

    Добавление кнопки и метки для граф.1

--- ChartExamples.py
+++ ChartExamples.py
@@ -2,6 +2,9 @@
 
 import tkinter as tk
 
+# Импорт внешних файлов
+import chart1
+
 # Функция закратия программмы
 def do_close():
     window.destroy()
@@ -12,8 +15,16 @@ window.geometry('450x450+300+200')
 window.title('Примеры построения графиков')
 
 # Добавление метки заголовка
-btnClose = tk.Label(window, text='Примеры построения графиков', font=('Helvetica', 16, 'bold'), fg='#0000cc')
-btnClose.place(x=55, y=25)
+lblTitle = tk.Label(window, text='Примеры построения графиков', font=('Helvetica', 16, 'bold'), fg='#0000cc')
+lblTitle.place(x=55, y=25)
+
+# Добавление кнопки и метки для графика 1
+btnChart1 = tk.Button(window, text='График 1', font=('Helvetica', 10, 'bold'), activebackground='gray85', command=chart1.plot_chart )
+btnChart1.place(x=40, y=115, width=90, height=30)
+
+lblChart1 = tk.Label(window, text='График синуса matplotlib')
+lblChart1.place(x=170, y=122)
+
 
 # Добавление кнопки закрытия программы
 btnClose = tk.Button(window, text='Закрыть', font=('Helvetica', 10, 'bold'), activebackground='gray85', command=do_close)
---
+++ chart1.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_chart():
+
+    # Data for plotting
+    t = np.arange(0.0, 2.0, 0.01)
+    s = 1 + np.sin(2 * np.pi * t)
+
+    fig, ax = plt.subplots()
+    ax.plot(t, s)
+
+    ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+        title='График синуса matplotlib')
+    ax.grid()
+
+    plt.show()
